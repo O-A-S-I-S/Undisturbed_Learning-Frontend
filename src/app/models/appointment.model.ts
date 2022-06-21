400c2f864ca2cd08b16a118d5137c143c110879d
Roberto Basauri <89091501+RobertoCarlosBasauriQuispe@users.noreply.github.com>
2022-06-21 18:38:49 -0500

    Create appointment.model.ts
    Creación del receptor del JSON de cita.

---
+++ src/app/models/appointment.model.ts
@@ -0,0 +1,12 @@
+export class Appointment {
+    id?: number;
+    start?: string;
+    end?: string;
+    causeDescription?: string;
+    comment?: string;
+    rating?: number;
+    psychopedagogistId?: number;
+    psychopedagogistName?: string;
+    studentId?: number;
+    studentName?:string;
+}
