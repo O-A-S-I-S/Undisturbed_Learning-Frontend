d66c460784248f28735b322888d3a70b779b3f03
Roberto Basauri <89091501+RobertoCarlosBasauriQuispe@users.noreply.github.com>
2022-06-21 18:29:26 -0500

    Update app.component.ts
    Variables globales para las rutas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Undisturbed-Learning-Frontend';
+  title = 'ULFrontEnd';
+  public appointmentId:number = 2;
+  public studentId: number = 4;
 }
