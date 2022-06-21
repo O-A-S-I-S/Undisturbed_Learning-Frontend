cce3725acfea833cd8063eb11dc3dadea7f1bce7
Roberto Basauri <89091501+RobertoCarlosBasauriQuispe@users.noreply.github.com>
2022-06-21 18:28:37 -0500

    Update app-routing.module.ts
    Colocando las rutas.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
+import { AppointmentsComponent } from './components/appointments/appointments.component';
+import { ReportsComponent } from './components/reports/reports.component';
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component:HomeComponent },
+  { path: 'student/:studentId', component:AppointmentsComponent},
+  { path: 'student/:studentId/reports/:appointmentId', component: ReportsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
